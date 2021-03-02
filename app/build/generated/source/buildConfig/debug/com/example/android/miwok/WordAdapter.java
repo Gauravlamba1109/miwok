a553e99f4fa0c6f1b5547f129c4a77b4656473a8
Gaurav Lamba <Gauravlamba1109@users.noreply.github.com>
2021-03-02 21:39:46 +0530

    custom adapter

---
+++ app/build/generated/source/buildConfig/debug/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,40 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //if the existing view is being reused check , otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+        Word currentword = getItem(position);
+        TextView miwokTextView=(TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentword.getMiwokTranslation());
+
+        TextView defaultTextView=(TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentword.getDefaultTranslation());
+
+        return listItemView;
+    }
+}
