f8580eb4f10fb3c3125611113ec18b58f497276f
Gaurav Lamba <Gauravlamba1109@users.noreply.github.com>
2021-03-02 21:38:00 +0530

    added word class and custom adapter

--- app/src/main/java/com/example/android/miwok/numbersActivity.java
+++ app/src/main/java/com/example/android/miwok/numbersActivity.java
@@ -17,23 +17,30 @@ public class numbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        ArrayList<String> words = new ArrayList<String>();
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
-        words.add("eleven");
+        /**
+         * we added the word class so now we can use the word class /entity inplace of string
+         * in our arraylist so that we can incoperate both the default and the miwok words
+         */
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        Word w = new Word("one","lutti");
+        words.add(w);
+        words.add(new Word("two","ottiko"));
+        words.add(new Word("three","tolookosu"));
+        words.add(new Word("four","oyyisa"));
+        words.add(new Word("five","massokka"));
+        words.add(new Word("six","temmokka"));
+        words.add(new Word("seven","kenekaku"));
+        words.add(new Word("eight","kawinta"));
+        words.add(new Word("nine","wo'e"));
+        words.add(new Word("ten","na'aacha"));
+
+
         //defining a array-adapter to recycle the view
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
-        //continue from here
+
 
     }
 }
